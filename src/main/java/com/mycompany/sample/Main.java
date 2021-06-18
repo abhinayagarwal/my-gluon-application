94da2fc8a0a26d8df8650b8e148ce190e0c62da9
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2021-06-18 08:40:51 +0530

    Fix import

--- src/main/java/com/mycompany/sample/Main.java
+++ src/main/java/com/mycompany/sample/Main.java
@@ -14,7 +14,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox
+import javafx.scene.layout.VBox;
 
 public class Main extends MobileApplication {
 
